5d7e9c48d91ed0e5b8b20f8af6408fc159db9cf2
Radium Zheng <ztl8702@msn.com>
2018-03-15 11:18:21 +1100

    fix small bugs, it's working now

--- ai_part_1.py
+++ ai_part_1.py
@@ -1,13 +1,11 @@
 
-// RULES
-    // "O" is white
-    // "@" is black
-    // "X" is corner 
-    // "-" is empty
+## RULES
+#  "O" is white
+# "@" is black
+# "X" is corner 
+# "-" is empty
     
 board = [[0 for x in range(0,8)] for y in range(0,8)]
-
-board =
 
 for i in range(0, len(board)):
     board[i] = input().strip().split(" ")
@@ -17,15 +15,6 @@ for i in range(0,8):
     print(board[i])
     
     
-    
-// iterate thought board
-
-// when looking at "O" OR "@"
-    // if (L,R,D,U) is == "-"
-        // then move++
-    // else if (L,R,D,U) is == "@" OR == "O"
-        // if there is space to jump to (space == "-")
-            // then move++
 DIRECTION = [
     [0, -1], #left
     [0, +1], #right
@@ -47,13 +36,13 @@ def isValidMove(x,y, direction):
     newX = x + DIRECTION[direction][0]
     newY = y + DIRECTION[direction][1]
     # if empty
-    if isWithBoard(newX,newY) and isEmpty(newX, newY):
+    if isWithinBoard(newX,newY) and isEmpty(newX, newY):
         return True
 
     # if can jump
     newX2 = newX + DIRECTION[direction][0]
     newY2 = newY + DIRECTION[direction][1]
-    if isWithBoard(newX2,newY2) and isEmpty(newX2,newY2) and board[newX,newY] in ['@', 'O']:
+    if isWithinBoard(newX2,newY2) and isEmpty(newX2,newY2) and board[newX][newY] in ['@', 'O']:
         return True
     return False
     
@@ -65,6 +54,9 @@ def countMoves(colour):
         for j in range(0, len(board)): 
             if board[i][j] == colour:
                 for direction in range(0,4):
-                    if isValidMove(x,y,direction):
+                    if isValidMove(i,j,direction):
                         possibleMoves = possibleMoves + 1
     return possibleMoves
+
+print(countMoves('O'))
+print(countMoves('@'))
