70c6d3874ff69cd7101a2076ae7631b8b77e6a7a
Radium Zheng <ztl8702@msn.com>
2018-03-20 16:58:27 +1100

    using Board in bfs

--- ai_part_1_bfs.py
+++ ai_part_1_bfs.py
@@ -7,44 +7,27 @@ def BFS(board):
 
     # store all nodes to visit in a queue
     queue = [board]
-
-    # store all visited nodes
-    explored = []
+    hashedBoardStates[board.getHashValue()] = True
 
     while queue:
         node = queue.pop(0)
-        if node not in explored:
-            explored.append(node)
+        # iterate through all pieces on board 
+        # and store board states at the back of queue
+        whitePieces = currentBoard.getAllPieces(Board.PIECE_WHITE)
 
-            # iterate through all pieces on board 
-            # and store board states at the back of queue
+        for p in whitePieces:
+            (i, j) = p
+        availableMoves = currentBoard.getAvailableMoves(i,j)
+        for move in availableMoves:
+            (newI, newJ) = move[1]
+            neighbour = currentBoard.makeMove(i,j, newI, newJ)
+            newBoardHash = hashBoard(neighbour)
 
-            for i in range(0, len(node)):
-                for j in range(0, len(node)):
-                    if node[i][j] == PIECE_WHITE:
-                        # check for all directions
-                        for direction in range(0, 4):
-                            # if not zero = we can move
-                            if isValidMove(node, i, j, direction) != 0:
-                                # if one = normal move
-                                if isValidMove(node, i, j, direction) == 1:
-                                    iDirection = i + DIRECTION[direction][0]
-                                    jDirection = j + DIRECTION[direction][1]
-
-                                # if two = jump move
-                                elif isValidMove(node, i, j, direction) == 2:
-                                    iDirection = i + DIRECTION[direction][0] * 2
-                                    jDirection = j + DIRECTION[direction][1] * 2
-
-                                neighbour = makeMove(i, j, iDirection, jDirection, board)
-                                newBoardHash = hashBoard(neighbour)
-
-                                # check if the current board state was seen before
-                                if (newBoardHash not in hashedBoardStates) or (hashedBoardStates[newBoardHash] == False):
-                                    hashedBoardStates[newBoardHash] = True
-                                    moves.append(((i, j), (iDirection, jDirection)))
-
-                                    queue.append(neighbour)
+            # check if the current board state was seen before
+            if (newBoardHash not in hashedBoardStates) or (hashedBoardStates[newBoardHash] == False):
+                hashedBoardStates[newBoardHash] = True
+                moves.append(move)
+                queue.append(neighbour)
 
     return False
 
