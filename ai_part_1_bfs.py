70bcf0b5bf91c3c6426f721d1fa18c658c93a9d3
Therrense Lua <tlua@student.unimelb.edu.au>
2018-03-20 17:25:19 +1100

    fixed bfs, fastest

--- ai_part_1_bfs.py
+++ ai_part_1_bfs.py
@@ -5,35 +5,51 @@ hashedBoardStates = {}
 
 def BFS(board):
 
+    def printMoves(moves):
+        '''
+        '''
+        for move in moves:
+            print(move[0], '->', move[1])
+
     # store all nodes to visit in a queue
-    queue = [board]
+    queue = [(board, [])]
     hashedBoardStates[board.getHashValue()] = True
 
     while queue:
-        node = queue.pop(0)
+        (node, previousMoves) = queue.pop(0)
         # iterate through all pieces on board 
         # and store board states at the back of queue
-        whitePieces = currentBoard.getAllPieces(Board.PIECE_WHITE)
+        whitePieces = node.getAllPieces(Board.PIECE_WHITE)
 
         for p in whitePieces:
             (i, j) = p
-        availableMoves = currentBoard.getAvailableMoves(i,j)
-        for move in availableMoves:
-            (newI, newJ) = move[1]
-            neighbour = currentBoard.makeMove(i,j, newI, newJ)
-            newBoardHash = hashBoard(neighbour)
+            availableMoves = node.getAvailableMoves(i,j)
+        
+            for move in availableMoves:
+                (newI, newJ) = move[1]
+                neighbour = node.makeMove(i,j, newI, newJ)
+                newBoardHash = neighbour.getHashValue()
 
-            # check if the current board state was seen before
-            if (newBoardHash not in hashedBoardStates) or (hashedBoardStates[newBoardHash] == False):
-                hashedBoardStates[newBoardHash] = True
-                moves.append(move)
-                queue.append(neighbour)
+                # check if the current board state was seen before
+                if (newBoardHash not in hashedBoardStates) or (hashedBoardStates[newBoardHash] == False):
+                    hashedBoardStates[newBoardHash] = True
+
+                    currentMoves = copy.deepcopy(previousMoves)
+                    currentMoves.append(move)
+
+                    queue.append((neighbour, currentMoves))
+
+                    if (neighbour.isWon(Board.PIECE_WHITE)):
+                        printMoves(currentMoves)
+                        return True
 
     return False
 
 
+previousMoves = []
+currentMoves = []
 
-moves = []
+
 oboard = Board()
 
 oboard.readInput()
@@ -43,8 +59,5 @@ if command == "Moves":
     print(oboard.countMoves(Board.PIECE_WHITE))
     print(oboard.countMoves(Board.PIECE_BLACK))
 else:
-    for i in range(1, 1000):
-        print("Trying depth ", i)
-        hashedBoardStates = {}
-        if (DFS(oboard,i)):
-            break
+    hashedBoardStates = {}
+    BFS(oboard)
