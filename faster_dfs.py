2c4026daf92f6c8a9c08bd7779d60b3f4b1ae119
Therrense Lua <tlua@student.unimelb.edu.au>
2018-03-29 20:32:22 +1100

    add faster_dfs

---
+++ faster_dfs.py
@@ -0,0 +1,72 @@
+import copy
+from common import Board
+
+# DFS called multiple times, so need to keep track globally
+hashedBoardStates = {}
+
+# search for winning solution
+# return True if found
+def DFS(board, ourPiece, depth):
+        
+    def printMoves():
+        # print all the moves made
+        for move in moves:
+            print(move[0], '->', move[1])
+
+    if depth == 0:
+        return False
+    if board.isWon(ourPiece):
+        printMoves()
+        return True
+
+    #  get limited/interesting search space
+    searchSpace = board.getInterestingCells(ourPiece)
+
+    ourPieces = board.getAllPieces(ourPiece)
+
+    for p in ourPieces:
+        (x, y) = p
+        availableMoves = board.getAvailableMoves(x, y)
+
+        for move in availableMoves:
+
+            # only search moves that are in search space
+            if (move[1] in searchSpace):
+
+                (newX, newY) = move[1]
+                newBoardState = board.makeMove(x, y, newX, newY, ourPiece)
+                newBoardHash = newBoardState.getHashValue()
+                
+                # check if the current state is in previous states
+                if (newBoardHash not in hashedBoardStates) or \
+                (hashedBoardStates[newBoardHash] < depth):
+                    # also consider fewer steps to reach the same state
+                    hashedBoardStates[newBoardHash] = depth
+                    moves.append(move)
+
+                    isFound = DFS(newBoardState, ourPiece, depth - 1)
+
+                    # backtrack
+                    moves.pop()
+                    if (isFound):
+                        return True
+    return False
+
+
+oboard = Board()
+
+oboard.readInput()
+command = input().strip()
+
+if command == "Moves":
+    print(oboard.countMoves(Board.PIECE_WHITE))
+    print(oboard.countMoves(Board.PIECE_BLACK))
+else:
+    for i in range(1, 1000):
+        # print("Trying depth ", i) # DEBUG
+        moves = []
+        
+        if (DFS(oboard, Board.PIECE_WHITE, i)):
+            break
+
+
