aa032be2ade6c93ba06c8f880b085208d638a3d5
Radium Zheng <ztl8702@users.noreply.github.com>
2018-05-11 13:32:34 +1000

    Remove Tree. Clean up code

--- ai/common/game_state_helpers.py
+++ ai/common/game_state_helpers.py
@@ -1,58 +1,66 @@
-from . import Board
+from . import Board, BoardStatus
 from typing import Type
 from .config import TURNS_BEFORE_SHRINK
 
+
 def get_successor_board_states(board, turnToPlay, side='@')-> Type[Board]:
-        if (turnToPlay <= 24):
-            # placing phase
-            if side == Board.PIECE_BLACK:
-                validYZone = range(2, 8)
-            else:
-                validYZone = range(0, 6)
-            moves = [(x, y)
-                     for (x, y) in board.get_empty_cells() if y in validYZone]
-
-            newStates = [((x, y), board.place_piece(x, y, side))
-                         for (x, y) in moves]
+    if (turnToPlay <= 24):
+        # placing phase
+        if side == Board.PIECE_BLACK:
+            validYZone = range(2, 8)
         else:
-            # Moving phase
-            ourPieces = board.get_all_pieces(side)
-            #print(ourPieces)
-            #if (len(ourPieces)==0):
-            #    print("no pieces")
-            #    board.print_board()
-            #    print(board.all_pieces)
-            #    print(board.all_pieces['O'])
-            #    print(board.all_pieces['@'])
-            #    print(board.all_pieces[side],'side',side, side=='O', side=='@')
-            #    print(list(board.all_pieces[side]))
-            moves = []
-            for (x, y) in ourPieces:
-                moves += board.getAvailableMoves(x, y)
+            validYZone = range(0, 6)
+        moves = [(x, y)
+                 for (x, y) in board.get_empty_cells() if y in validYZone]
 
-            if turnToPlay in TURNS_BEFORE_SHRINK:
-                newStates = [
-                    (((fromX, fromY), (toX, toY)),
-                     board.make_move(fromX, fromY, toX, toY, side).shrink()
-                     )
-                    for ((fromX, fromY), (toX, toY)) in moves
-                ]
-            else:
-                newStates = [
-                    (((fromX, fromY), (toX, toY)),
-                     board.make_move(fromX, fromY, toX, toY, side)
-                     )
-                    for ((fromX, fromY), (toX, toY)) in moves
-                ]
+        newStates = [((x, y), board.place_piece(x, y, side))
+                     for (x, y) in moves]
+    else:
+        # Moving phase
+        ourPieces = board.get_all_pieces(side)
+        moves = []
+        for (x, y) in ourPieces:
+            moves += board.getAvailableMoves(x, y)
 
-        return newStates
+        if turnToPlay in TURNS_BEFORE_SHRINK:
+            newStates = [
+                (((fromX, fromY), (toX, toY)),
+                 board.make_move(fromX, fromY, toX, toY, side).shrink()
+                 )
+                for ((fromX, fromY), (toX, toY)) in moves
+            ]
+        else:
+            newStates = [
+                (((fromX, fromY), (toX, toY)),
+                 board.make_move(fromX, fromY, toX, toY, side)
+                 )
+                for ((fromX, fromY), (toX, toY)) in moves
+            ]
+
+    return newStates
+
+
+opponents_mapping = {'@': 'O', 'O': '@'}
 
 
 def get_opponent_colour(ourColour):
     '''
     Returns the opponent piece type/colour
     '''
-    if (ourColour == Board.PIECE_WHITE):
-        return Board.PIECE_BLACK
+    # using dict is about 10% faster than if-else statement
+    return opponents_mapping[ourColour]
+
+
+def fake_status(board):
+    """
+    Returns a fake "Game Status" (i.e. who won? is it a tie?)
+    based on the number of pieces left.
+    """
+    whitePieces = board.get_all_pieces(Board.PIECE_WHITE)
+    blackPieces = board.get_all_pieces(Board.PIECE_BLACK)
+    if (len(whitePieces) > len(blackPieces)):
+        return BoardStatus.WHITE_WON
+    elif (len(blackPieces) > len(whitePieces)):
+        return BoardStatus.BLACK_WON
     else:
-        return Board.PIECE_WHITE
+        return BoardStatus.TIE
